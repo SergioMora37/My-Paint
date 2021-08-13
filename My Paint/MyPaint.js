07acda774a5578e76f6364ba03031b56373177e1
Sergio Mora <85002550+SergioMora37@users.noreply.github.com>
2021-08-13 16:34:21 -0500

    Add files via upload

---
+++ My Paint/MyPaint.js
@@ -0,0 +1,56 @@
+/*  Declaramos las variables, almacenando el id gracias al metodo getElementById */
+var lienzo = document.getElementById("area_dibujo");
+var color = document.getElementById("color");
+var anchito = document.getElementById("ancho_trazo");
+var boton = document.getElementById("botoncito");
+var papel = lienzo.getContext("2d");
+var colorcito = "black";
+
+var estado = 0;
+
+ /*METODOS escuchador de eventos, junto la funcion correspondiente */
+boton.addEventListener("click", clearLienzo);
+color.addEventListener("click", defcolor);
+anchito.addEventListener("click", ancho);
+
+/* FUNCIONES*/ 
+function defcolor(c){
+    colorcito = c;
+    papel.strokeStyle = color.value;
+}
+
+function clearLienzo(){
+    papel.clearRect(0, 0, 800, 500);
+}
+
+function ancho(){
+    papel.lineWidth = anchito.value;
+}
+
+function dibujarLinea(evento)
+{  
+    x = evento.layerX;
+    y = evento.layerY;
+    if(estado == 1){
+        papel.lineTo(x, y);
+        console.log("dibujando");
+        papel.stroke();
+        console.log( "Posicion x" + x + " Posicion y" + y);
+        }
+}
+lienzo.addEventListener("mousemove", dibujarLinea);
+
+lienzo.addEventListener("mousedown", function()
+{
+    estado = 1;
+    papel.beginPath();
+    papel.moveTo(x, y);
+    console.log("Mouse presionado");
+});
+
+lienzo.addEventListener("mouseup", function()
+{
+    estado = 0;
+    console.log("Mouse soltado");
+    papel.closePath();
+});
